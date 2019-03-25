5f491eb902ad4e38fe44e93b50cde4cc199fcb3b
GreenTGreenT <s5901012630091@email.kmutnb.ac.th>
2019-03-26 02:17:02 +0700

    ER to Relational Mapping

--- search/models.py
+++ search/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Word(models.Model):
+    word_text = models.CharField(max_length=50)
+
+class Website(models.Model):
+    title_text = models.CharField(max_length=100)
+    link_text = models.CharField(max_length=10000)
+    word = models.ManyToManyField(Word, through='WordWebsite')
+
+class WordWebsite(models.Model):
+    word = models.ForeignKey(Word, on_delete=models.CASCADE)
+    website = models.ForeignKey(Website, on_delete=models.CASCADE)
+    count = models.IntegerField(default=0)
