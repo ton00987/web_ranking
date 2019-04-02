affe320ed8cb639ad98332d034f53835ea68a5fd
Jirawat Yuktawathin <yuk.jirawat@gmail.com>
2019-04-02 07:17:17 -0700

    Website model can specify root url and word indexing

--- search/models.py
+++ search/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 
 class Word(models.Model):
-    word_text = models.CharField(max_length=50)
+    word = models.CharField(max_length=50, default=None, db_index=True)
 
 class Website(models.Model):
-    title_text = models.CharField(max_length=100)
-    link_text = models.CharField(max_length=10000)
+    title = models.CharField(max_length=100)
+    url = models.CharField(max_length=10000)
+    root = models.ForeignKey('self', default=None, on_delete=models.CASCADE)
     word = models.ManyToManyField(Word, through='WordWebsite')
 
 class WordWebsite(models.Model):
