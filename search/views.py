4cb02b22ea88296e5719cf27ff61bbad152d543f
Krittanat Ananchailikit <theton0087@hotmail.com>
2019-04-08 04:41:55 +0700

    Query web and show on template

--- search/views.py
+++ search/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from search.models import Word, Website, WordWebsite
 
 def index(request):
-    return render(request, 'index.html')
+    search = request.GET.get('search', '')
+    webs = WordWebsite.objects.select_related('word', 'website').filter(word__word=search).order_by('-count')
+    return render(request, 'index.html', {'webs': webs})
