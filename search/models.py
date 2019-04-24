bb16d675ce910a41814672c5d83def160083df67
GreenTGreenT <s5901012630091@email.kmutnb.ac.th>
2019-04-24 16:54:33 +0700

    Remodel database

--- search/models.py
+++ search/models.py
@@ -4,15 +4,16 @@ from django.db import models
 class Word(models.Model):
     word = models.CharField(max_length=50, default=None, db_index=True)
 
-# Foreign key should be null if url is root
 class Website(models.Model):
     title = models.CharField(max_length=100)
     url = models.CharField(max_length=10000)
-    root = models.ForeignKey('self', default=None, on_delete=models.CASCADE, null=True)
-    word = models.ManyToManyField(Word, through='WordWebsite')
+    ref = models.ManyToManyField('self', default=None, null=True, symmetrical=False, db_index=True)
+    word = models.ManyToManyField(Word, through='Have')
     click = models.IntegerField(default=0)
 
-class WordWebsite(models.Model):
+class Have(models.Model):
     word = models.ForeignKey(Word, on_delete=models.CASCADE)
     website = models.ForeignKey(Website, on_delete=models.CASCADE)
     count = models.IntegerField(default=0)
+
+    
--- search/views.py
+++ search/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect
-from search.models import Word, Website, WordWebsite
+from search.models import Word, Website, Have
 
 def index(request):
     search = request.GET.get('search', '')
-    join_table = WordWebsite.objects.select_related('word', 'website')
+    join_table = Have.objects.select_related('word', 'website')
     find_word = join_table.filter(word__word=search)
     sort_webs = find_word.order_by('-website__click', '-count')
     return render(request, 'index.html', {'sort_webs': sort_webs})
