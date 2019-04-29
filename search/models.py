9b6be462e5932503481692d703835331490418cd
Krittanat Ananchailikit <theton0087@hotmail.com>
2019-04-29 22:10:25 +0700

    URL indexing and change order of website

--- search/models.py
+++ search/models.py
@@ -6,15 +6,13 @@ class Word(models.Model):
 
 class Website(models.Model):
     title = models.CharField(max_length=100, null=True)
-    url = models.CharField(max_length=10000)
+    url = models.CharField(max_length=10000, db_index=True)
     date = models.DateField(default='2000-01-01')
     click = models.IntegerField(default=0)
-    ref = models.ManyToManyField('self', default=None, null=True, symmetrical=False, db_index=True)
+    ref = models.ManyToManyField('self', default=None, null=True, symmetrical=False)
     word = models.ManyToManyField(Word, through='Have')
 
 class Have(models.Model):
     word = models.ForeignKey(Word, on_delete=models.CASCADE)
     website = models.ForeignKey(Website, on_delete=models.CASCADE)
     count = models.IntegerField(default=0)
-
-    
--- search/views.py
+++ search/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, redirect
+from django.db.models import F, Count
 from search.models import Word, Website, Have
 
 def index(request):
+    ''' Search by word order by reference, click and count '''
     search = request.GET.get('search', '')
     join_table = Have.objects.select_related('word', 'website')
     find_word = join_table.filter(word__word=search)
-    sort_webs = find_word.order_by('-website__click', '-count')
+    sort_webs = find_word.annotate(weight=Count('website__website')*20 \
+        + F('website__click')*4 + F('count')).order_by('-weight')
     return render(request, 'index.html', {'sort_webs': sort_webs})
 
 def click(request):
